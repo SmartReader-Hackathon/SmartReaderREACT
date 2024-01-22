f4d9b0c8d4c1fde2f4dde1f61b803ecd5774c5f5
stelltriz <stellbea@icloud.com>
2024-01-21 21:51:06 -0300

    ss

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -1,46 +1,86 @@
 import React, { useState } from "react";
 import "./cadastro.css";
-import { logo, roxo } from "../../assets";
+import { logo } from "../../assets";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
-const Cadastro = () => (
-        <div>
-            <div className='container'>
-                <div className='cad'>
-                    <p1>CaP/ETS</p1>
-                    <p>Hackathon 1.2024</p>
-                    <Link to="/Cadastro">
-                        <button className="new">New Register</button>
-                    </Link>
-                </div>
+const Cadastro = () => {
+  const [name, setFirstName] = useState("");
+  const [sobrenome, setLastName] = useState("");
+  const [edv, setEdv] = useState("");
+  const [area, setAllowedDoors] = useState("");
 
-                <Link to="/">
-                    <div className='title'>
-                        <p1>smart</p1>
-                        <p className="gradient">Reader</p>
-                        <img src={logo} alt="Logo"></img>
-                    
-                    </div>
-                </Link>
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/user", {
+        name: String(name),
+        sobrenome: String(sobrenome),
+        edv: parseInt(edv),
+        area: String(area),
+      });
+    }
+     catch (error) {
+      console.error("Erro ao registrar usuário:", error);
+    }
+  };
 
-                <div className="cadastrando">
-                    <div className="registration">Registration</div>
-
-                    <div className="label">First Name:</div>
-                    <input className="nome"></input>
-
-                    <div className="label">Last Name:</div>
-                    <input className="last"></input>
-
-                    <div className="label">EDV:</div>
-                    <input className="edvcadastro"></input>
-
-                    <div className="label">Allowed Doors:</div>
-                    <input className="doors"></input>
-
-                    <button className="cada">Register</button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className='container'>
+        <div className='cad'>
+          <p1>CaP/ETS</p1>
+          <p>Hackathon 1.2024</p>
+          <Link to="/Cadastro">
+            <button className="new">New Register</button>
+          </Link>
         </div>
-    );
+
+        <Link to="/">
+          <div className='title'>
+            <p1>smart</p1>
+            <p className="gradient">Reader</p>
+            <img src={logo} alt="Logo"></img>
+          </div>
+        </Link>
+
+        <div className="cadastrando">
+          <div className="registration">Registration</div>
+
+          <div className="label">First Name:</div>
+          <input
+            className="nome"
+            value={name}
+            onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+
+          <div className="label">Last Name:</div>
+          <input
+            className="last"
+            value={sobrenome}
+            onChange={(e) => setLastName(e.target.value)}
+          ></input>
+
+          <div className="label">EDV:</div>
+          <input
+            className="edvcadastro"
+            value={edv}
+            onChange={(e) => setEdv(e.target.value)}
+          ></input>
+
+          <div className="label">Allowed Doors:</div>
+          <input
+            className="doors"
+            value={area}
+            onChange={(e) => setAllowedDoors(e.target.value)}
+          ></input>
+
+          <button className="cada" onClick={handleRegister}>
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Cadastro;
